a74669e34223a4a1a6604aa473cbcba74834e85b
Eduardo Gurgel <eduardo@gurgel.me>
2014-07-27 22:16:45 +1200

    Add test to Connection when connecting to unknown host

--- test/connection_test.exs
+++ test/connection_test.exs
@@ -23,4 +23,7 @@ defmodule ConnectionTest do
     assert :ok = Connection.close(conn)
   end
 
+  test "open connection on unknown host" do
+    assert {:error, :unknown_host} = Connection.open "amqp://unknown_host"
+  end
 end
