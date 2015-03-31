1131e7220989afcdd986cce6c27c47977a65159c
Paulo Almeida <paulo.almeida@timeboxed.co.ao>
2015-03-31 22:50:06 +0100

    Bump version Update deps Include OTP 17.1 in travis build

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AMQP.Mixfile do
 
   def project do
     [app: :amqp,
-     version: "0.1.0",
+     version: "0.1.1-dev",
      elixir: "~> 1.0.0",
      description: description,
      package: package,
