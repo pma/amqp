62b384b7818c632db21afd92609f7e9aab755b1d
Tatsuya Ono <ononoma@gmail.com>
2018-11-09 19:20:43 +0000

    Clarify the default value is atom

--- lib/amqp/connection.ex
+++ lib/amqp/connection.ex
@@ -33,7 +33,7 @@ defmodule AMQP.Connection do
     * `:frame_max` - The frame_max handshake parameter (defaults to `0`);
     * `:heartbeat` - The hearbeat interval in seconds (defaults to `10`);
     * `:connection_timeout` - The connection timeout in milliseconds (defaults to `60000`);
-    * `:ssl_options` - Enable SSL by setting the location to cert files (defaults to `none`);
+    * `:ssl_options` - Enable SSL by setting the location to cert files (defaults to `:none`);
     * `:client_properties` - A list of extra client properties to be sent to the server, defaults to `[]`;
     * `:socket_options` - Extra socket options. These are appended to the default options. \
                           See http://www.erlang.org/doc/man/inet.html#setopts-2 and http://www.erlang.org/doc/man/gen_tcp.html#connect-4 \
