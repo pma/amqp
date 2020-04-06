423eb336ae6e3b2a3820d75807388d47647711d6
Frederico Berger <fredericoberger@gmail.com>
2020-04-06 15:22:02 -0300

    Receive options on queue subscribe and pass to basic consume

--- lib/amqp/queue.ex
+++ lib/amqp/queue.ex
@@ -202,9 +202,9 @@ defmodule AMQP.Queue do
   """
   @spec subscribe(Channel.t(), Basic.queue(), (String.t(), map -> any)) ::
           {:ok, String.t()} | Basic.error()
-  def subscribe(%Channel{} = channel, queue, fun) when is_function(fun, 2) do
+  def subscribe(%Channel{} = channel, queue, fun, options \\ []) when is_function(fun, 2) do
     consumer_pid = spawn(fn -> do_start_consumer(channel, fun) end)
-    Basic.consume(channel, queue, consumer_pid)
+    Basic.consume(channel, queue, consumer_pid, options)
   end
 
   defp do_start_consumer(channel, fun) do
