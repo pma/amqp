f0ccd4c9f24e107992cfff13384e4e022c18f43c
Tatsuya Ono <ononoma@gmail.com>
2020-12-12 16:02:22 +0000

    mix format

--- lib/amqp/selective_consumer.ex
+++ lib/amqp/selective_consumer.ex
@@ -6,13 +6,14 @@ defmodule AMQP.SelectiveConsumer do
   alias AMQP.SelectiveConsumer
   @behaviour :amqp_gen_consumer
 
-  defstruct [consumers: %{}, unassigned: :undefined, monitors: %{}, default_consumer: :none]
+  defstruct consumers: %{}, unassigned: :undefined, monitors: %{}, default_consumer: :none
+
   @type t :: %SelectiveConsumer{
-    consumers: %{String.t() => pid},
-    unassigned: pid | :undefined,
-    monitors: %{pid => {integer, reference}},
-    default_consumer: pid | :none
-  }
+          consumers: %{String.t() => pid},
+          unassigned: pid | :undefined,
+          monitors: %{pid => {integer, reference}},
+          default_consumer: pid | :none
+        }
 
   @doc """
   Ported from :amqp_selective_consumer.register_default_consumer/2.
@@ -39,11 +40,13 @@ defmodule AMQP.SelectiveConsumer do
       case nowait do
         true when tag == :undefined or is_nil(tag) or byte_size(tag) == 0 ->
           :no_consumer_tag_specified
+
         _ when is_binary(tag) and byte_size(tag) >= 0 ->
           case resolve_consumer(tag, status) do
             {:consumer, _} -> :consumer_tag_in_use
             _ -> :ok
           end
+
         _ ->
           :ok
       end
@@ -58,7 +61,7 @@ defmodule AMQP.SelectiveConsumer do
         {:ok, %{status | unassigned: pid}}
 
       {error, true} ->
-        {:error, error, status};
+        {:error, error, status}
 
       {_error, false} ->
         # Don't do anything (don't override existing consumers), the server will close the channel with an error.
@@ -67,7 +70,12 @@ defmodule AMQP.SelectiveConsumer do
   end
 
   @impl true
-  def handle_consume_ok(basic_consume_ok(consumer_tag: tag) = consume_ok, _consume, %{unassigned: pid} = status) when is_pid(pid) do
+  def handle_consume_ok(
+        basic_consume_ok(consumer_tag: tag) = consume_ok,
+        _consume,
+        %{unassigned: pid} = status
+      )
+      when is_pid(pid) do
     c = Map.put(status.consumers, tag, pid)
     m = add_to_monitors(status.monitors, pid)
 
@@ -81,18 +89,19 @@ defmodule AMQP.SelectiveConsumer do
 
   @impl true
   def handle_cancel(basic_cancel(nowait: true), %{default_consumer: :none}) do
-    exit(:cancel_nowait_requires_default_consumer);
+    exit(:cancel_nowait_requires_default_consumer)
   end
 
   def handle_cancel(basic_cancel(nowait: nowait) = cancel, status) do
     case nowait do
-      true  -> {:ok, do_cancel(cancel, status)}
+      true -> {:ok, do_cancel(cancel, status)}
       false -> {:ok, status}
     end
   end
 
   defp do_cancel(cancel, status) do
     tag = tag(cancel)
+
     case Map.fetch(status.consumers, tag) do
       {:ok, consumer} ->
         c = Map.delete(status.consumers, tag)
@@ -185,7 +194,7 @@ defmodule AMQP.SelectiveConsumer do
   defp deliver_to_consumer_or_die(tag, message, status) do
     case resolve_consumer(tag, status) do
       {:consumer, pid} -> send(pid, message)
-      {:default, pid} -> send(pid, message);
+      {:default, pid} -> send(pid, message)
       _error -> exit(:unexpected_delivery_and_no_default_consumer)
     end
   end
@@ -233,7 +242,9 @@ defmodule AMQP.SelectiveConsumer do
                cluster_id: cluster_id
              ),
            payload: payload
-         ), _ctx) do
+         ),
+         _ctx
+       ) do
     {:basic_deliver, payload,
      %{
        consumer_tag: consumer_tag,
@@ -278,6 +289,7 @@ defmodule AMQP.SelectiveConsumer do
     case Map.fetch(monitors, pid) do
       :error ->
         Map.put(monitors, pid, {1, :erlang.monitor(:process, pid)})
+
       {:ok, {count, mref}} ->
         Map.put(monitors, pid, {count + 1, mref})
     end
