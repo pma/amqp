1867bf00945dc7f245003ab6a38a2321ce9ebd8e
Matt Silbernagel <matt@silbernagel.dev>
2025-06-10 13:07:07 -0400

    Allow advanced channel parameters - specifically channel numbers - when opening a channel (#241)
    * Add ability to pass the channel number to Channel.open/3
    
    * Add tests to validate
    
    * update to use a 3-arity function that allows for a channel number
    
    * Add channel number to struct

--- lib/amqp/channel.ex
+++ lib/amqp/channel.ex
@@ -6,8 +6,15 @@ defmodule AMQP.Channel do
   alias AMQP.{Connection, Channel, SelectiveConsumer}
 
   defstruct [:conn, :pid, :custom_consumer]
-  @type t :: %Channel{conn: Connection.t(), pid: pid, custom_consumer: custom_consumer() | nil}
+
+  @type t :: %Channel{
+          conn: Connection.t(),
+          pid: pid,
+          channel_number: channel_number() | nil,
+          custom_consumer: custom_consumer() | nil
+        }
   @type custom_consumer :: {module(), args :: any()}
+  @type channel_number :: non_neg_integer()
 
   @doc """
   Opens a new Channel in a previously opened Connection.
@@ -16,10 +23,29 @@ defmodule AMQP.Channel do
   implementation. The consumer must implement the `:amqp_gen_consumer` behavior
   from `:amqp_client`. See `:amqp_connection.open_channel/2` for more details
   and `AMQP.DirectConsumer` for an example of a custom consumer.
+
+  Allows for a channel number to be passed as a third argument. See `:amqp_connection.open_channel/3`
   """
-  @spec open(Connection.t(), custom_consumer | nil) :: {:ok, Channel.t()} | {:error, any}
-  def open(%Connection{} = conn, custom_consumer \\ {SelectiveConsumer, self()}) do
+  @spec open(Connection.t(), custom_consumer() | nil, channel_number() | nil) ::
+          {:ok, Channel.t()} | {:error, any}
+  def open(conn, custom_consumer \\ {SelectiveConsumer, self()}, channel_number \\ nil)
+
+  def open(%Connection{} = conn, {_, _} = custom_consumer, channel_number)
+      when is_nil(channel_number) do
     do_open_channel(conn, custom_consumer)
+  end
+
+  def open(%Connection{} = conn, nil, nil) do
+    do_open_channel(conn, nil)
+  end
+
+  def open(%Connection{} = conn, custom_consumer, nil) do
+    do_open_channel(conn, custom_consumer)
+  end
+
+  def open(%Connection{} = conn, custom_consumer, channel_number)
+      when is_number(channel_number) do
+    do_open_channel(conn, custom_consumer, channel_number)
   end
 
   @doc """
@@ -47,6 +73,32 @@ defmodule AMQP.Channel do
     case :amqp_connection.open_channel(conn.pid, custom_consumer) do
       {:ok, chan_pid} ->
         {:ok, %Channel{conn: conn, pid: chan_pid, custom_consumer: custom_consumer}}
+
+      error ->
+        {:error, error}
+    end
+  end
+
+  defp do_open_channel(conn, nil, channel_number) do
+    case :amqp_connection.open_channel(conn.pid, channel_number) do
+      {:ok, chan_pid} ->
+        {:ok, %Channel{conn: conn, pid: chan_pid, channel_number: channel_number}}
+
+      error ->
+        {:error, error}
+    end
+  end
+
+  defp do_open_channel(conn, custom_consumer, channel_number) do
+    case :amqp_connection.open_channel(conn.pid, channel_number, custom_consumer) do
+      {:ok, chan_pid} ->
+        {:ok,
+         %Channel{
+           conn: conn,
+           pid: chan_pid,
+           custom_consumer: custom_consumer,
+           channel_number: channel_number
+         }}
 
       error ->
         {:error, error}
--- test/channel_test.exs
+++ test/channel_test.exs
@@ -14,4 +14,18 @@ defmodule ChannelTest do
     assert {:ok, chan} = Channel.open(meta[:conn])
     assert :ok = Channel.close(chan)
   end
+
+  test "open channel with custom_consumer only", meta do
+    assert {:ok, chan} = Channel.open(meta[:conn], {AMQP.DirectConsumer, self()})
+    assert {AMQP.DirectConsumer, _pid} = chan.custom_consumer
+    assert :ok = Channel.close(chan)
+  end
+
+  test "open channel with channel number and custom_consumer", meta do
+    assert {:ok, chan} =
+             Channel.open(meta[:conn], {AMQP.DirectConsumer, self()}, 12)
+
+    assert {AMQP.DirectConsumer, _pid} = chan.custom_consumer
+    assert :ok = Channel.close(chan)
+  end
 end
